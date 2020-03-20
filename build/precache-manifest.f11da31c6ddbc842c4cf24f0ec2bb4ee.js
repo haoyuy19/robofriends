21f8e58274e299e21c91f84916d7b18f88eea4f0
Haoyu Yang <haoyuy19@gmail.com>
2020-03-20 15:03:53 -0700

    Added RoboFriends Project

---
+++ build/precache-manifest.f11da31c6ddbc842c4cf24f0ec2bb4ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77dbf89e2e51e035f4a90079f596659c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50eede5cc3f311992174",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "ac4d1047530e932e60da",
+    "url": "/static/css/main.7c1877d8.chunk.css"
+  },
+  {
+    "revision": "50eede5cc3f311992174",
+    "url": "/static/js/2.84821b79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84821b79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac4d1047530e932e60da",
+    "url": "/static/js/main.9c7fa0d9.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
